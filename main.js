abc3d511448d0cae1346346731cf69971e321b46
LEBVAS <149606003+LEBVAS@users.noreply.github.com>
2023-11-01 17:47:37 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,9 @@ ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', {delay: 500});
 ScrollReveal().reveal('.cards-banner-one', {delay: 500});
 ScrollReveal().reveal('.cards-banner-two', {delay: 500});
+
+
+
+
+
+
